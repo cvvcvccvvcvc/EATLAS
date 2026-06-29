d2add6a11c10b75b64466f58bfb2cd51e498e741
IL <1wdyz11@gmail.com>
2026-06-29 17:56:46 +0500

    Remove local scratch artifacts

--- scratch2.py
+++
@@ -1,6 +0,0 @@
-import pysam
-
-clinvar = pysam.VariantFile("/Users/cvv/code/Bioinformatics/course-work/course-work-code/data/clinvar.vcf.gz")
-for rec in clinvar.fetch("1", 1000000, 2000000):
-    print(rec.chrom, rec.pos, rec.ref, rec.alts, rec.info.get("CLNSIG"))
-    break
